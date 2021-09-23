171a9fe279c2fdcd6f74b37750d1f4cbfc495547
NataliaPC <natalia.pujol@gmail.com>
2021-09-23 16:04:06 +0200

    Bugfix in dosver()

--- src/dos_dosver.c
+++ src/dos_dosver.c
@@ -1,4 +1,5 @@
 #include "dos.h"
+#include "debug.h"
 
 
 char dosver(void) __naked {
@@ -11,28 +12,35 @@ char dosver(void) __naked {
     ld ix, #0
 
     ld c,#DOSVER
-    DOSCALL
+    call 0xF37D       ; BDOS (upper memory DOSCALL)
 
     or  a
-    jr  z,check_dos2$
+    jr  z,check_dos1$
     ld  b,#0          ; unknown DOS
     jr  ret_version$
 
-  check_dos2$:
-    ld  a,b
+  check_dos1$:
+    ld  a,b           ; B<2 if is MSX-DOS 1
     cp  #2
-    jr  z,check_nextor$
+    jr  nc,check_dos2nextor$
     ld  b,#1          ; is MSX-DOS 1
+    jr  ret_version$
 
-  check_nextor$:
-    ld  a,i           ; Nextor: IXh must contain '1'
+  check_dos2nextor$:
+    push ix           ; Nextor: IXh must contain '1'
+    pop hl
+    ld  a,h
     dec a
-    jr  nz,ret_version$ 
-    ld  b,#3          ; is NextorDOS
+    jr  z,is_nextor$
+    ld  b,#2          ; is MSXDOS 2
+    jr  ret_version$
+  
+  is_nextor$:         ; is NextorDOS
+    ld  b,#3
 
   ret_version$:
-    ld h, #0x00
-    ld l, b
+    ld  h, #0x00
+    ld  l, b
 
     pop ix
     ret
