e685e0d5ce2eae09973c7a2bb2ac3ad76075c773
nataliapc <natalia.pujol@gmail.com>
2025-05-02 02:15:37 +0200

    Bugfix fgets with EOF

--- src/dos2_fgets.c
+++ src/dos2_fgets.c
@@ -8,8 +8,8 @@ char* dos2_fgets(char *str, uint16_t size, FILEH fh)
 
 	while (--size) {
 		ret = dos2_fread(p, 1, fh);
+		if (ret == (0xff00|ERR_EOF) && str != p) break;	// ERR_EOF with content
 		if (*p++ == '\n') break;
-		if (ret == 0xffc7) break;	// ERR_EOF
 		if (ret & 0xff00) return 0;
 	}
 	*p = '\0';
